d1f3f53a2c996074e7b4051cdb72d6d44b028fbb
Luqmanul Hakim Zulkifli <49841356+mlhakimz@users.noreply.github.com>
2022-05-26 00:21:09 +0800

    Create board_config.h for Maker Zero SAMD21

---
+++ boards/cytron_maker_zero_samd21/board_config.h
@@ -0,0 +1,17 @@
+#ifndef BOARD_CONFIG_H
+#define BOARD_CONFIG_H
+
+#define VENDOR_NAME "Cytron Technologies"
+#define PRODUCT_NAME "Maker Zero SAMD21"
+#define VOLUME_LABEL "ZEROBOOT"
+
+#define BOARD_ID "SAMD21G18A-Zero-v0"
+
+#define USB_VID 0x04D8
+#define USB_PID 0xE799
+
+#define LED_PIN PIN_PA17
+#define LED_TX_PIN PIN_PA27
+#define LED_RX_PIN PIN_PB03
+
+#endif
